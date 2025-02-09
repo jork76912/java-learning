183078a62f87d003f04d5061d92fbb0d30c87bb9
jork76912 <2488369083@qq.com>
2025-02-09 21:58:07 +0800

    第一天提交：完成基础语法和面向对象练习

---
+++ main_01.java
@@ -0,0 +1,21 @@
+package com.wawa.helloworld;
+
+public class main_01 {
+    public static void main(String[] args)
+    {
+        Calculator calculator = new Calculator();
+        double x = 10;
+        double y = 20;
+        System.out.println(calculator.add(x,y));
+        System.out.println(calculator.sub(x,y));
+        System.out.println(calculator.mul(x,y));
+        System.out.println(calculator.div(x,y));
+        try {
+            double result = calculator.div(10, 0); // 这里会触发异常,会跳过下面的代码
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.err.println("Caught an exception: " + e.getMessage());//err与out独立的流
+        }
+        System.out.println(calculator.add(3,5));
+    }
+}
