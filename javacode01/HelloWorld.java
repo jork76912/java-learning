47139c577af477d0855729e183256d00f380c7a0
jork76912 <2488369083@qq.com>
2025-02-10 23:52:47 +0800

    Day1提交：完成基础语法和面向对象练习

---
+++ javacode01/HelloWorld.java
@@ -0,0 +1,47 @@
+package com.java_learning.Javacode01;
+
+
+public class HelloWorld {
+          public static void main(String[] args){
+              System.out.println("Hello World");
+//              byte b = 10;  //一个字节
+//              short s = 100;  //两个字节
+//              int A = 23;   //4个字节
+//              long L = 888L;  //8个字节
+//              char c = 'A';  //两个字节
+//              String name = "张三";
+//              float f = 7.2f;  //4个字节
+//              final double PI = 3.14; //8个字节
+//              boolean flag = true;  //一位
+             // System.out.println(flag);
+//              System.out.println(A);
+//              System.out.println(name);
+//              System.out.println(PI);
+
+//               条件语句
+//        int score = 70;
+//        if(score >= 60){
+//            System.out.println("你及格啦\n");
+//        }
+//        else{
+//            System.out.println("很遗憾，你没通过！");
+//        }
+//
+////              循环语句
+//              for(int i = 0;i <= 5; i++)
+//                  System.out.println(i);
+
+//              switch语句
+//              char grade = 'E';
+//              switch(grade){
+//                  case 'A' :
+//                      System.out.println("优秀");
+//                      break;
+//                  case 'B' :
+//                      System.out.println("良好");
+//                  default:
+//                      System.out.println("未知等级");
+//              }
+
+          }
+}
